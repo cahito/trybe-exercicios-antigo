bc3105acdc78d8153f4f2fe6b20ce02923fd6b58
cahito <cahito@gmail.com>
2021-11-26 18:29:22 -0300

    Preparação para exercícios

---
+++ fundamentos/Bloco04/scripts.js
@@ -0,0 +1,100 @@
+/*const myName = 'Caio';
+const birthCity = 'Sanja';
+let birthYear = 1976;
+
+birthYear = 2030;
+
+console.log(myName, birthCity, birthYear);
+*/
+
+/*let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+*/
+/* const base = 5;
+let height = 8;
+
+const area = base * height;
+
+console.log(area);
+
+const perimeter = 2 * base + 2 * height;
+
+console.log(perimeter); */
+
+/* const nota = 55;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} else if (nota < 80 && nota >=60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovada(o)")
+}; */
+
+/* const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+} */
+
+/* const conditionOne = true;
+const conditionTwo = false;
+
+console.log(conditionOne && conditionTwo); */
+
+/* const cenouras = true;
+const leite = true;
+const arroz = true;
+const feijao = true;
+
+const listaDeCompras = cenouras && leite && arroz && feijao; */
+
+/* console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false */
+
+/* const currentHour = 3;
+let message = '';
+
+if (currentHour >=22) {
+    console.log("Não deveríamos comer nada, é hora de dormir");
+} else if (currentHour >=18 && currentHour < 22) {
+    console.log("Rango da noite, vamos jantar :D");
+} else if (currentHour >=14 && currentHour < 18) {
+    console.log("Vamos fazer um bolo pro café da tarde?");
+} else if (currentHour >=11 && currentHour < 14) {
+    console.log("Hora do almoço!!!");
+} else if (currentHour >=4 && currentHour < 11) {
+    console.log("Hmmm, cheiro de café recém passado")
+} else {
+    console.log("ZZZzzz...");
+}; */
+
+/* const bebidaPrincipal =  'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+} */
+
+/* console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false */
