a66d9eb07e30f381e0e7c8af1568dd1a18494118
cahito <cahito@gmail.com>
2021-11-29 18:12:43 -0300

    Exercícios pré aula

---
+++ fundamentos/Bloco04/4.2/exercicio1.js
@@ -0,0 +1,28 @@
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+menu.push('Contato');
+
+console.log(menu);
+*/
+
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let i = 0; i < groceryList.length; i += 1) {
+    console.log(groceryList[i]);
+} */
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+    console.log(name);
+    /* for (let letra of name) {
+        console.log(letra);
+    } */
+}
---
+++ fundamentos/Bloco04/4.2/scripts.js
@@ -0,0 +1,82 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+// 3
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+tasksList.unshift('Dormir até acordar')
+console.log(tasksList);
+
+
+tasksList.pop();  // remove a última tarefa
+tasksList.shift(); //remove o primeiro elemento
+console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião' ]
+
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
+
+// 1
+
+// Elementos de repetição "FOR"
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+
+for (let index = 0; index < cars.length; index += 1) {
+    console.log(cars[index]);
+  }
+
+
+  let numeros = [1,2,3,4,5];
+  for(let numero of numeros) {
+    console.log(numero);
+  }
+  
+  // resultado: 
+  //1
+  //2
+  //3
+  //4
+  //5
+
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+
+
+let arrOfNumbers = [10, 20, 30];
+for (let sum of arrOfNumbers) {
+  sum += 1;
+  console.log(sum);
+}
+// 11
+// 21
+// 31
+
+console.log(arrOfNumbers);
+// Resultado: [10, 20, 30]
