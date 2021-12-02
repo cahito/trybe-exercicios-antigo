7f157ced08e6a94df20e2511b8debc79efe602b2
cahito <cahito@gmail.com>
2021-12-02 11:31:03 -0300

    Exercícios do 4.4- Parte I

--- fundamentos/Bloco04/4.4/scripts.js
+++ fundamentos/Bloco04/4.4/scripts.js
@@ -1,4 +1,4 @@
-let info = {
+/* let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -6,4 +6,61 @@ let info = {
 
 // Bem-vinda, Margarida
 
-console.log('Bem-vinda, ' + info.personagem);
+//console.log('Bem-vinda, ' + info.personagem);
+
+// Inserir uma caraterística a mais no objeto
+
+info['recorrente'] = 'Sim';
+
+// Utilizar for/in para mostrar as chaves
+
+ for ( let keys in info) {
+  console.log(keys);
+} 
+
+// Utilizar for/in para mostrar os valores das chaves
+
+ for (let keys in info) {
+  console.log(info[keys]);
+}
+
+// Criar o Tio Patinhas e imprimir uma relação dos objetos
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {
+  if (info[key] === info2[key]) {
+    console.log("Ambos concorrentes");
+  } else {
+    console.log (info[key] + ' e ' + info2[key]);
+  }
+}
+*/
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos['0'].titulo + "'");
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
